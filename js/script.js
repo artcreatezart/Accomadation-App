69e1aee323d81bfc45f1eed28e306d1f5c9cf68f
artcreatezart <abbie@Abbies-MBP.hub>
2024-07-02 23:41:28 +1200

    filtering working and working on calculations

--- js/script.js
+++ js/script.js
@@ -14,8 +14,8 @@ const accomadation = [{
     maxStay: "5",
     bedrooms: "2",
     bathrooms: "1",
-    parking: "no",
-    kidFriendly: "yes",
+    parking: "false",
+    kidFriendly: "true",
     location: "Wellington",
     accomadationInfo: "Here at Cippy Hotel we pride ourselves on service and quality. A perfect location, for a perfect stay. At $157 a night you can get a cosy room with 2 bedrooms, a seperate bathroom, and a spacious lounge and Kitchen. Plus Free Wifi. <br> <br>We offer an inclusive breakfast buffet from 5am to 9am don't miss out.",
     accomadationLongitude: "174.7786201350749",
@@ -35,8 +35,8 @@ const accomadation = [{
     maxStay: "10",
     bedrooms: "1",
     bathrooms: "1",
-    parking: "no",
-    kidFriendly: "no",
+    parking: "false",
+    kidFriendly: "false",
     location: "Auckland",
     accomadationInfo: "Traveling alone and looking for accomadation thats both in the perfect location and afforable? Bolas's Hostle is the place for you. <br> <br> Here you share a space with other visitors, you are provided with a bed and space to store you items, and you share the rest of the space such as bathrooms, showers, kitchen and lounges with the others. A perfect place if your looking for somewhere afforable, love getting to know Guests and needing somewhere to stay in between adventures.",
     accomadationLongitude: "174.757619453614",
@@ -56,8 +56,8 @@ const accomadation = [{
     maxStay: "10",
     bedrooms: "2",
     bathrooms: "2",
-    parking: "yes",
-    kidFriendly: "no",
+    parking: "true",
+    kidFriendly: "false",
     location: "Napier",
     accomadationInfo: "Hawkes Lane Motel is in a supreme spot around shops and awesome senic outlooks. While your here you can easily walk to many cafes, resturants, shops and some of the most beautiful walks. <br> <br> Here you will get 2 bedrooms, with 3 beds, 2 bathrooms and common area. You will have access to the motels pool and pool tables which is a perfect place to pass time.",
     accomadationLongitude: "172.64084119719655",
@@ -77,8 +77,8 @@ const accomadation = [{
     maxStay: "15",
     bedrooms: "3",
     bathrooms: "2",
-    parking: "yes",
-    kidFriendly: "yes",
+    parking: "true",
+    kidFriendly: "true",
     location: "Wellington",
     accomadationInfo: "This house if perfect for those looking for a private place to stay on your vacation where you can have the house to yourself and perfect for a family, friends or when multiple Guests are staying. <br><br> At the Jiolan House there are 3 bedrooms, 3 beds, a kitchen, 2 bathrooms, 2 common areas, a dining room, outdoor area and a kitchen.",
     accomadationLongitude: "174.77209913051644",
@@ -98,8 +98,8 @@ const accomadation = [{
     maxStay: "18",
     bedrooms: "4",
     bathrooms: "2",
-    parking: "no",
-    kidFriendly: "yes",
+    parking: "false",
+    kidFriendly: "true",
     location: "Napier",
     accomadationInfo: "This house is so convient for location with everything you could possibly need within walking distance. Dreamy Aquatic House is a 2 storey house with plenty of room and a beautifu view of the gorgeous water.<br><br> Includes 4 bedrooms, 5 beds, 2 bathrooms, 1 kitchen, 2 lounge areas, a garage, outdoor space and 1 parking space. Imagine youself waking up on a brisk cool morning with a hot coffee in your hands as you glance over the crisp clear blue water amongst Napier.",
     accomadationLongitude: "176.9169491447278",
@@ -120,8 +120,8 @@ const accomadation = [{
     maxStay: "10",
     bedrooms: "3",
     bathrooms: "2",
-    parking: "no",
-    kidFriendly: "no",
+    parking: "false",
+    kidFriendly: "false",
     location: "Christchurch",
     accomadationInfo: "Luxury Stay Motel is a a luxury experience without the overpriced aspect. Enjoy a magnificent view from our 3 story building, imagine waking up the the warmth of the beutiful morning sun to start off you adventure of the day. <br><br> This hotel room is spacious with 3 bedroom, 4 beds, 2 bathroom, 1 lounge area and a kitchen area. Perfect when looking for a place to stay on your adventure for 2-4 Guests.",
     accomadationLongitude: "172.62164982955926",
@@ -271,8 +271,6 @@ $(document).ready(function () {
         const selectedLocation = $('#location').val();
         const guests = parseInt($('#guests').val(), 10) || 0;
         const selectedType = $('#type').val();
-        const kidFriendly = $('#kidFriendly').val();
-        const parking = $('#carPark').val();
         const diffDays = calculateDays();
         console.log(diffDays);
         // const budgetFilterPrice = calculateBudgetFilterPrice();
@@ -280,17 +278,14 @@ $(document).ready(function () {
 
         const filteredAccomadations = accomadation.filter(accomadation => {
             return (accomadation.location === selectedLocation || selectedLocation === '') &&
-                (parseInt(accomadation.minGuests, 10) <= guests) &&
-                (parseInt(accomadation.maxGuests, 10) >= guests) &&
-                (accomadation.kidFriendly === kidFriendly || kidFriendly === '') &&
-                (accomadation.parking === parking || parking === '') &&
+                (accomadation.minGuests <= guests) &&
+                (accomadation.maxGuests >= guests) &&
                 (accomadation.type === selectedType || selectedType === '') &&
                 (accomadation.minStay <= diffDays) &&
                 (accomadation.maxStay >= diffDays);
-                // (accomadation.minPrice <= budgetFilterPrice) &&
-                // (accomadation.maxPrice >= budgetFilterPrice)
+            // (accomadation.minPrice <= budgetFilterPrice) &&
+            // (accomadation.maxPrice >= budgetFilterPrice)
         });
-
 
         console.log(filteredAccomadations);
         displayAccomadations(filteredAccomadations);
@@ -316,9 +311,7 @@ $(document).ready(function () {
     //     const minPrice = $("#minPrice").select("getPrice");
     //     const maxPrice = $("#maxPrice").select("getPrice");
 
-    // }
-
-
+    // 
 
     function displayAccomadations(filteredAccomadations) {
         const accomadationsPerPageSmall = 1;
@@ -338,11 +331,10 @@ $(document).ready(function () {
         let accomadationsPerPage = getAccomadationsPerPage();
 
         const swiperWrapper = $('.swiper-wrapper');
-        // empty the swiper
         swiperWrapper.empty();
 
         for (let i = 0; i < filteredAccomadations.length; i += accomadationsPerPage) {
-            const slide = $('<div class="swiper-slide"></div>'); // each slide
+            const slide = $('<div class="swiper-slide"></div>');
 
             for (let j = i; j < i + accomadationsPerPage && j < filteredAccomadations.length; j++) {
                 const accomadation = filteredAccomadations[j];
@@ -377,10 +369,10 @@ $(document).ready(function () {
                     </div>
                 </div>
                 <div class="card-info-right-container">
-                    <div class="extra-info">
-                        <i class="fa-solid fa-car" id="carIcon"></i>
-                        <i class="fa-solid fa-child" id="childIcon"></i>
-                    </div>
+                <div class="extra-info">
+                ${accomadation.parking ? '<i class="fa-solid fa-car" id="carIcon"></i>' : ''}
+                ${accomadation.kidFriendly ? '<i class="fa-solid fa-child" id="childIcon"></i>' : ''}
+              </div>
                     <h2>${accomadation.price} / night</h2>
                     <button class="primary-button" id="seeMoreAccomadationButton">See More<i class="fa-solid fa-arrow-right"></i></button>
                 </div>
@@ -391,10 +383,9 @@ $(document).ready(function () {
             }
             swiperWrapper.append(slide);
         }
-        // Destroy the swiper instance
+        // Destroy the swiper
         swiper.destroy(true, true);
-        
-        // initalise a new one
+
         swiper = new Swiper('.swiper', {
             direction: 'horizontal',
             pagination: {
@@ -402,9 +393,113 @@ $(document).ready(function () {
             }
         });
 
-        // Rebuild Fullpage to see the new slides
+        // Rebuild Fullpage 
         fullpage_api.reBuild();
 
+        $("#seeMoreAccomadationButton").click(function () {
+            const accomadationId = $(this).data('id');
+            console.log($(this).data('id'));
+
+            populateSelectedOutput(accomadationId);
+            fullpage_api.moveTo(2, 2);
+        });
+
+        function populateSelectedOutput(selectedId) {
+            const outputAccomadationSelected = $("#accomadationSelected");
+            const slideOutputHtml = `<div class="left-accomadation-selected-container">
+            <div class="accomadation-selected-img"></div>
+            <div class="left-accomadation-selected-info">
+                <div class="left-accomadation-selected-name-info">
+                    <h1>${accomadation.name}</h1>
+                    <div class="left-accomadation-selected-location-info">
+                        <h5>${accomadation.type}</h5>
+                        <i class="fa-solid fa-location-dot"></i>
+                        <h4>${accomadation.location}</h4>
+                    </div>
+                </div>
+                <p>${accomadation.accomadationInfo}</p>
+                <div class="left-accomadation-selected-bottom-info-section">
+                    <div class="left-accomadation-selected-ammenities">
+                        <div class="left-left-accomadation-selected-ammenities">
+                            <div class="guests-info">
+                                <i class="fa-solid fa-user"></i>
+                                <h6>${accomadation.minGuests} - ${accomadation.maxGuests} Guests</h6>
+                            </div>
+                            <div class="nights-info">
+                                <i class="fa-solid fa-moon"></i>
+                                <h6>${accomadation.minStay} - ${accomadation.maxStay} Nights</h6>
+                            </div>
+                            <div class="selected-accomadation-ammenites-info">
+                                <div class="bedroom-info">
+                                    <h6>${accomadation.bedrooms}</h6>
+                                    <i class="fa-solid fa-bed"></i>
+                                </div>
+                                <div class="bathrooms-info">
+                                    <h6>${accomadation.bathrooms}</h6>
+                                    <i class="fa-solid fa-toilet"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="left-right-accomadation-selected-ammenities">
+                            <div class="parking-info">
+                                ${accomadation.parking ? '<i class="fa-solid fa-car" id="carIcon"></i>'
+                                :
+                                ''}
+                                ${accomadation.parking ? '<h6>CarPark <br> Available</h6>'
+                                :
+                                ''}
+                            </div>
+
+                            <div class="kid-friendly-info">
+                                ${accomadation.kidFriendly ? '<i class="fa-solid fa-child" id="childIcon"></i>' : ''}
+                                ${accomadation.kidFriendly ? '<h6>Kid <br> Friendly</h6>' : ''}
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+                <div class="right-accomadation-selected-bottom-info-section">
+                    <h2>${accomadation.price} / night</h2>
+                    <button class="primary-button" id="bookButton" data-id="${selectedId}>Book</button>
+                </div>
+
+            </div>
+        </div>
+        <div class="vertical-line"></div>
+        <div class="meal-plan-container">
+            <div class="meal-plan-header">
+                <h1>Meal Plan</h1>
+                <i class="fa-solid fa-utensils"></i>
+            </div>
+            <p>Here we provide multiple different meal options if you wish to have them. We can also
+                cater to any dietery restrictions just give us a call for any inquires. <br> <span
+                    class="meal-highlight-comment">*Meals are priced per guest for entire stay</span>
+            </p>
+            <div class="meal-plan-select">
+                <div class="meal-plan-option">
+                    <input type="checkbox" id="breakfastCheck" value="breakfast">
+                    <label for="breakfast">Breakfast - $15</label>
+                </div>
+                <div class="meal-plan-option">
+                    <input type="checkbox" id="lunchCheck" value="lunch">
+                    <label for="lunch">Lunch - $20</label>
+                </div>
+                <div class="meal-plan-option">
+                    <input type="checkbox" id="dinnerCheck" value="dinner">
+                    <label for="dinner">Dinner - $30</label>
+                </div>
+                <div class="meal-plan-option">
+                    <input type="checkbox" id="noMealCheck" value="no">
+                    <label for="no">No Thanks</label>
+                </div>
+                <button class="primary-button">Add Meal Plan</button>
+            </div>
+        </div>`;
+
+            outputAccomadationSelected.empty();
+            outputAccomadationSelected.append(slideOutputHtml);
+        }
     }
 
     // Date picker formats
@@ -432,6 +527,14 @@ $(document).ready(function () {
     $("#backButton").click(function (e) {
         e.preventDefault();
         fullpage_api.moveTo(2, 0);
+    });
+
+    $("#bookButton").click(function () {
+        const accomadationId = $(this).data('id');
+        console.log($(this).data('id'));
+        
+        populateSelectedOutput(accomadationId);
+        fullpage_api.moveTo(2, 4);
     });
 
     // BUTTON FUCNTIONS ENDS
